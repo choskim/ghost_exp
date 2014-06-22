e687803ba1574dc7138195fc15504320ebb89628
Cho S. Kim <4cho.kim@gmail.com>
2014-06-21 18:29:56 -0700

    Updates production URL

--- config.js
+++ config.js
@@ -48,7 +48,7 @@ config = {
     // When running Ghost in the wild, use the production environment
     // Configure your URL and mail settings here
     production: {
-        url: 'http://my-ghost-blog.com',
+        url: 'http://chochomon.azurewebsites.net',
         mail: {},
         database: {
             client: 'sqlite3',
